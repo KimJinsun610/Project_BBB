a20fb2ca1baf295978269dec8d884413ca270b99
Jinsun Kim <realsun@tukorea.ac.kr>
2026-02-03 18:04:06 +0900

    moving action

--- Project_BBB/Source/Project_BBB/Character/BBBCharacterPlayer.h
+++ Project_BBB/Source/Project_BBB/Character/BBBCharacterPlayer.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Character/BBBCharacterBase.h"
+#include "InputActionValue.h"
 #include "BBBCharacterPlayer.generated.h"
 
 /**
@@ -16,8 +17,11 @@ class PROJECT_BBB_API ABBBCharacterPlayer : public ABBBCharacterBase
 
 public:
 	ABBBCharacterPlayer();
-	
 
+protected:
+	virtual void BeginPlay() override;
+	
+//================================================
 protected: // Camera Section
 	
 	// 카메라 스플링암
@@ -27,4 +31,29 @@ protected: // Camera Section
 	// Carmera
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UCameraComponent> FollowCamera;
+
+
+//================================================
+// Input Section
+
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
 };
--- Project_BBB/Source/Project_BBB/Project_BBB.Build.cs
+++ Project_BBB/Source/Project_BBB/Project_BBB.Build.cs
@@ -12,7 +12,8 @@ public class Project_BBB : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
